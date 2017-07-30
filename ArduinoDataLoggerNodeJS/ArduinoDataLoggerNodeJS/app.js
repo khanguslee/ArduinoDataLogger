fb20ff408a75a023aa21a8965e04559fc52397ae
khanguslee <angus.lee809@gmail.com>
2017-07-30 12:50:41 +1000

    Initial code to test for tilt sensor.

--- ArduinoDataLoggerNodeJS/ArduinoDataLoggerNodeJS/app.js
+++ ArduinoDataLoggerNodeJS/ArduinoDataLoggerNodeJS/app.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('Hello world');
+/*
+    Pin Layout:
+        Orange:     5v
+        Brown:      Pin 8
+        Red:        GND
+*/
+
+var johnnyFive = require("Johnny-Five");
+var arduinoBoard = new johnnyFive.Board();
+
+arduinoBoard.on("ready", function() {
+    var tilt = new johnnyFive.Sensor.Digital(8);
+
+    tilt.on("change", function () {
+        if (this.value) {
+            console.log("TILT");
+        }
+    });
+});
