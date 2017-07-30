c34e73ffa7a79a059915b5df9cbecef4780c841b
khanguslee <angus.lee809@gmail.com>
2017-07-30 13:31:57 +1000

    Added ability to detect if on or off. Code can detect if the sensor has just been turned on or off.

--- ArduinoDataLoggerNodeJS/ArduinoDataLoggerNodeJS/app.js
+++ ArduinoDataLoggerNodeJS/ArduinoDataLoggerNodeJS/app.js
@@ -7,15 +7,49 @@
         Red:        GND
 */
 
+// Initialise Johnny-Five
 var johnnyFive = require("Johnny-Five");
 var arduinoBoard = new johnnyFive.Board();
 
-arduinoBoard.on("ready", function() {
+arduinoBoard.on("ready", function () {
+    console.log("Board ready!");
     var tilt = new johnnyFive.Sensor.Digital(8);
+    var sensorCount = 0;
+    var sensorFlag = false, prevSensorFlag = false;
 
+    // When sensor changes value
     tilt.on("change", function () {
-        if (this.value) {
-            console.log("TILT");
-        }
+        sensorCount = 0;
+        sensorFlag = true;
+        console.log("TILTING!");
     });
+
+    // Continuously loops
+    
+    var timeoutValue = 100;
+    tilt.on("data", function () {
+        console.log(sensorCount);
+
+        // Sensor just started turning on
+        if (sensorFlag & !prevSensorFlag)
+        {
+            prevSensorFlag = true;
+            console.log("Vibration started.");
+        }
+
+        // Sensor just turned off
+        if (!sensorFlag && prevSensorFlag)
+        {
+            prevSensorFlag = false;
+            console.log("Vibration stopped.");
+        }
+
+        // Sensor reaches timeout value
+        if (sensorCount == timeoutValue)
+        {
+            sensorCount = 0;
+            sensorFlag = false;
+        }
+        sensorCount++;
+    })
 });
